36dd8fd7f0fa829ddcdcab82a664511d5997582c
asus <aaa@aaa.aaa>
2020-05-06 23:25:17 +0500

    index.blade.php

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,56 @@
+@include('layouts.nav')
+<section class="welcome">
+	<div class="hotel">
+		<h1>Our Hotel</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		   <span class="border1"></span>
+
+	</div>
+</section>
+
+
+
+<section class="about">
+   <div class="team-section">
+		<h1>THE OWNERS OF THE HOTEL</h1>
+	    <span class="border"></span>
+		   <div class="ps">
+             <a href="#"><img src="{{ asset('pictures/Eldor.jpg') }}" alt=""></a>
+	          <a href="#"><img src="{{ asset('pictures/Rustam.jpg') }}" alt=""></a>
+      	    <a href="#"><img src="{{ asset('pictures/Husniddin.jpg') }}" alt=""></a>
+       	    <a href="#"><img src="{{ asset('pictures/Sardor.jpg') }}" alt=""></a>
+             <a href="#"><img src="{{ asset('pictures/Dilkhayot.jpg') }}" alt=""></a>
+             <a href="#"><img src="{{ asset('pictures/Tokhir.jpg') }}" alt=""></a>
+		   </div>
+   </div>
+      <div class="names">
+         <h4>Obidov Eldorjon</h4>
+      </div>
+
+      <div class="names1">
+         <h4>Nasrullaev Rustam</h4>
+      </div>
+
+      <div class="names2">
+         <h4>Khusainov Khusniddin</h4>
+      </div>
+
+      <div class="names3">
+         <h4>Rakhimjonov Sardorjon</h4>
+      </div>
+
+      <div class="names4">
+         <h4>Akhmedov Dilkhayot</h4>
+      </div>
+
+      <div class="names5">
+         <h4>Khurbonov Tokhir</h4>
+      </div>
+   </section>
+</body>
+</html>

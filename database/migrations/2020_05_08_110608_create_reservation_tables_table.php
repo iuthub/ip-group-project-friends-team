607ca81fad43033906d5917e44841ae83fabcae3
asus <aaa@aaa.aaa>
2020-05-09 00:31:23 +0500

    Bars & Restaurant & blog

---
+++ database/migrations/2020_05_08_110608_create_reservation_tables_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReservationTablesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservation_tables', function (Blueprint $table) {
+            $table->id();
+            $table->string('day');
+            $table->string('hour');
+            $table->string('name');
+            $table->string('tel_number');
+            $table->string('people');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservation_tables');
+    }
+}
--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -14,23 +14,24 @@
       }
    </style>
 </head>
-   <body>
-     <header>
-  	      <div class="logo">
-		      <img src="{{ asset('pictures/Ligo3.png') }} ">
-	      </div>
-            <ul>
-               <li class="active"><a href="#">HOME</a></li>
-	            <li><a href="#">BOOKING ROOMS</a></li>
-	            <li><a href="#">RESTAURANT & BARS & BANQUETS</a></li>
-	            <li><a href="#">SERVICES</a></li>
-	            <li class="dropdown">
-	           	<a href="javascript:void(0)" class="dropbtn">ABOUT & GALLARY</a>
-	            <div class="dropdown-content">
-              	   <a href="#">PHOTOS</a>
-              	   <a href="#">VIDEOS</a>
-	            </div>
-	            </li>
-	            <li><a href="#">CONTACT US</a></li>
-            </ul>
-         </header>
+<body>
+ <header>
+   <div class="logo">
+   <img src="{{ asset('pictures/Ligo3.png') }} ">
+   </div>
+     <ul>
+       <li class="active"><a href="#">HOME</a></li>
+       <li><a href="#">BOOKING ROOMS</a></li>
+       <li><a href="#">RESTAURANT & BARS & BANQUETS</a></li>
+       <li><a href="#">SERVICES</a></li>
+       <li class="dropdown">
+       	 <a href="javascript:void(0)" class="dropbtn">ABOUT & GALLARY</a>
+           <div class="dropdown-content">
+        	   <a href="#">PHOTOS</a>
+        	   <a href="#">VIDEOS</a>      
+           </div>
+       </li>
+       <li><a href="#">CONTACT US</a></li>
+     </ul>
+ </header>
+</body>

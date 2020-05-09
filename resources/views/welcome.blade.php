dc6a4eb33637cbeebb2cc8d0d060121120fec839
asus <aaa@aaa.aaa>
2020-05-09 19:18:56 +0500

    contant from Tohir

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,7 +39,7 @@
                        <a href="#">VIDEOS</a>      
                    </div>
                </li>
-               <li><a href="#">CONTACT US</a></li>
+               <li><a href="/contact">CONTACT US</a></li>
              </ul>
          </header>
 
